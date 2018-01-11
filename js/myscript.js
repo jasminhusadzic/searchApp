19c1508c19916f1112ed8aa96449fff7e8bbf4c3
Jasmin Husadzic <jasmin.husadzic@gmail.com>
2018-01-11 18:59:19 +0100

    saarch
    search app

---
+++ js/myscript.js
@@ -0,0 +1,16 @@
+function mojafunkcija(data){
+    var results = [];
+    element = document.getElementById('tb_name');
+    var key = element.value != "" ? key = element.value.toString() : key = null;
+    if (key === null){
+        console.log('cao');
+    }else{
+        var regex =  new RegExp(key, 'i');
+        for ( var i=0; i < data.length; i++){
+            if ((data[i].pNumber.search(regex) != -1) || (data[i].regNumber.search(regex) != -1) || (data[i].phone.search(regex) != -1)){
+                results.push(data[i]);			
+            }
+        }
+        console.log(results);
+    }
+}
